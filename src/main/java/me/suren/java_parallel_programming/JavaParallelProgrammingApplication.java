ca8c87794b7b007b9d36e242f9e276666b3f9dbe
Surendra Raut <surendra.raut@gmail.com>
2024-08-25 00:23:36 -0400

    Initial commit.

---
+++ src/main/java/me/suren/java_parallel_programming/JavaParallelProgrammingApplication.java
@@ -0,0 +1,13 @@
+package me.suren.java_parallel_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaParallelProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaParallelProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/suren/java_parallel_programming/JavaParallelProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suren.java_parallel_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaParallelProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
