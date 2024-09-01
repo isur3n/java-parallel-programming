11203d04ed8b2f5052efce55e3ebb9024d52bda8
Surendra Raut <surendra.raut@gmail.com>
2024-08-31 23:26:34 -0400

    Removed unwanted imports.

--- src/main/java/me/suren/java_parallel_programming/config/ApiConfig.java
+++ src/main/java/me/suren/java_parallel_programming/config/ApiConfig.java
@@ -1,7 +1,6 @@
 package me.suren.java_parallel_programming.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/me/suren/java_parallel_programming/controller/CompletableFutures.java
+++ src/main/java/me/suren/java_parallel_programming/controller/CompletableFutures.java
@@ -3,7 +3,6 @@ package me.suren.java_parallel_programming.controller;
 import lombok.extern.slf4j.Slf4j;
 import me.suren.java_parallel_programming.task.RandomDelay;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
